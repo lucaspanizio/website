f475e0d6f254da69b794bfc1e69eb9a3a3653bad
Lucas Panizio <lucas_panizio@hotmail.com>
2026-01-25 19:33:38 -0300

    :tada: feat: commit inicial
    Páginas home e resume iniciais.

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  base: '/website/',
+  publicDir: 'public',
+  output: 'static',
+  outDir: './dist',
+  build: {
+    assets: 'astro',
+  },
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontSize: {
+        xxs: 'var(--text-xxs)',
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: true,
+  },
+};
